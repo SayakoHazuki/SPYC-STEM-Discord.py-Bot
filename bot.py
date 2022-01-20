4aebf3d9dc399b8d1d8713ab7508229d99c02cda
Kai Kamf <74895715+Kai9073@users.noreply.github.com>
2022-01-21 07:40:54 +0800

    Update bot.py (added comments)

--- bot.py
+++ bot.py
@@ -15,10 +15,12 @@ import json
 client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
+# on bot ready
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
+# on test command
 @bot.command()
 async def test(ctx):
     await ctx.send('test')
@@ -60,6 +62,7 @@ def getAssignmentList():
             print('No courses found.')
             return
 
+        # Get assignment lists for all courses
         courseWorkList = []
         for course in courses:
             assignments = service.courses().courseWork().list(
@@ -67,13 +70,14 @@ def getAssignmentList():
             courseWorkList.append(assignments)
             print('Retrieved assignment list for {}'.format(course['name']))
 
+        # converts to list of assignments(dicts)
         assignmentList = []
-
         for courseWork in courseWorkList:
             if courseWork:
                 for work in courseWork["courseWork"]:
                     assignmentList.append(work)
 
+        # sort the list by due date/time
         sortedAssignments = sorted(lf,
                     key=lambda d: '{}{}{}{}{}'.format(d['dueDate']['year'],
                                                       f"{d['dueDate']['month']:02d}",
@@ -81,8 +85,8 @@ def getAssignmentList():
                                                       f"{d['dueTime']['hours'] if 'hours' in d['dueTime'] else 23:02d}",
                                                       f"{d['dueTime']['minutes'] if 'minutes' in d['dueTime'] else 59:02d}") if 'dueDate' in d else '999999999999', reverse=False)
 
+        # get submission states for all assignments in the list
         mySubmissions = []
-
         print('processing assignments:')
         for assignment in sortedAssignments:
             assignmentState = service.courses().courseWork().studentSubmissions().list(
@@ -90,6 +94,7 @@ def getAssignmentList():
             mySubmissions.append(assignmentState)
             print(assignment["title"])
 
+        # remove all submitted work from sortedAssignments[]
         for assignment in mySubmissions:
             if (assignment["studentSubmissions"][0]["state"] == 'RETURNED') or (assignment["studentSubmissions"][0]["state"]) == 'TURNED_IN':
                 assignmentId = assignment["studentSubmissions"][0]["courseWorkId"]
@@ -103,14 +108,14 @@ def getAssignmentList():
         with open('l4.json', 'w') as l4Json:
             l4Json.write('[{}]'.format(','.join(resultsStrArr)))
 
+    # on HttpError
     except HttpError as error:
         print('An error occurred: %s' % error)
 
+# load secrets (json)
 f = open('secrets.json')
- 
-# returns JSON object as
-# a dictionary
 secrets = json.load(f)
 
+# run the bot
 client.run(secrets["token"])
 
