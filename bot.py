c0ab18048b955b6fdc0a8e55e91fd82a991ccd84
Kai9073 <74895715+Kai9073@users.noreply.github.com>
2022-01-25 20:07:36 +0800

    Update timetable command & minor fixes

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
 # ============= imports =============
 
+from datetime import datetime
+import logging
 import json
 import os.path
+import re
 from sqlite3 import Timestamp
 from webbrowser import get
 import discord
 from discord.ext import commands, tasks
 
 from classroom import authAndGetService, fetchAssignments, timestampFromDue
-from timetable import getLessonList
+from timetable import getDayOfCycle, getLessonList
 
-import logging
+configFile = open('config.json')
+config = json.load(configFile)
+
+
+# For debugging only
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -19,6 +26,7 @@ handler = logging.FileHandler(
 handler.setFormatter(logging.Formatter(
     '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
+
 # ============ Bot Config ============
 
 intents = discord.Intents().all()  # Specify the bot intents
@@ -52,37 +60,50 @@ async def assignments(ctx):
     if os.path.exists('savedAssignments.json'):
         with open('savedAssignments.json', 'r') as savedAssignments:
             assignments = json.load(savedAssignments)
+
     # Get Assignment List and Add Fields
     else:
-        ctx.send('初次獲取資料需時約2分鐘, 請稍後重新使用指令')
+        return await ctx.send('初次獲取資料需時約2分鐘, 請稍後重新使用指令')
 
     if assignments == 'No courses found':
-        return ctx.send('No courses found')
+        return await ctx.send('找不到課程')
 
     # Add the first 24 assignments to the embed as fields
     for assignment in assignments[:24]:
         dueDatetime = ''
+
         if 'dueDate' in assignment:
             dueDatetime = timestampFromDue(assignment)
         else:
-            dueDatetime = 'No Due Date'
+            dueDatetime = '無截止日期'
+
         assignmentsEmbed.add_field(
             name=assignment["title"], value='截止時間: {}\n[開啟作業]({})'.format(dueDatetime, assignment["alternateLink"]), inline=False)
-    await ctx.send(embed=assignmentsEmbed)
+
+    return await ctx.send(embed=assignmentsEmbed)
 
 
 @bot.command()
-async def timetable(ctx, arg1 = 'None', arg2 = 'None'):
-    if (arg1 == 'None' and arg2 == 'None') :
-        return await ctx.send('請輸入班別以及Day')
-    
-    param = {'class_': arg1, 'day': arg2}
+async def timetable(ctx, arg1='', arg2=''):
+    if arg1 and not re.match(r'[A-H]', arg1):
+        return await ctx.send('不存在 Day {}'.format(arg1))
+
+    arg1 = arg1 or getDayOfCycle()
+    arg2 = arg2 or config["class"]
+
+    if not arg1:
+        return await ctx.send('發生了預期外的錯誤')
+
+    if arg1 == '/':
+        return await ctx.send('本日為學校假期')
+
+    param = {'day': arg1, 'class_': arg2,
+             'date': datetime.now().strftime('%d %B, %Y')}
     lessons = getLessonList(param['class_'], param['day'])
 
     if lessons[0] == None:
-        return ctx.send('ValueError : {}'.format(lessons[1]))
+        return await ctx.send('ValueError : {}'.format(lessons[1]))
 
-    print(lessons)
     embedFields = []
     i = 0
 
@@ -92,7 +113,9 @@ async def timetable(ctx, arg1 = 'None', arg2 = 'None'):
         i += 1
 
     timetableEmbed = discord.Embed(
-        title='{class_}班 Day {day} 時間表'.format(**param)
+        title='{class_}班時間表'.format(**param),
+        description='{date}\t\nDay {day}'.format(**param),
+        color=0x03A4EC
     )
 
     for field in embedFields:
