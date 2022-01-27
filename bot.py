bfbf39421eaabdc7a6756f264acdfd13fce4543e
MA Mr <83592482+KCMa@users.noreply.github.com>
2022-01-27 17:40:52 +0000

    env

--- bot.py
+++ bot.py
@@ -10,8 +10,9 @@ from tkinter import FALSE
 import discord
 from discord.ext import commands, tasks
 
-from classroom import authAndGetService, fetchAssignments, timestampFromDue
 from timetable import getDayOfCycle, getLessonList
+
+from env import fetchEnvData
 
 configFile = open('config.json')
 config = json.load(configFile)
@@ -39,61 +40,35 @@ service = None  # Will be used to store Google Classroom API Service
 
 # =========== Bot Events ===========
 
-@tasks.loop(minutes=10)  # Repeat every 10 minutes
-async def fetchAssignmentsTask(args):
-    await fetchAssignments(args)  # fetch Assignments
+# @tasks.loop(minutes=10)  # Repeat every 10 minutes
+# async def fetchAssignmentsTask(args):
+#     await fetchAssignments(args)  # fetch Assignments
 
 
 @bot.event  # On bot ready
 async def on_ready():
     print('Logged in as {0.user}'.format(bot))
     # Authorize Google Classroom API and get the API Service
-    service = await authAndGetService()
+    # service = await authAndGetService()
     # Start fetching assignments every 10 minutes
-    fetchAssignmentsTask.start(service)
+    # fetchAssignmentsTask.start(service)
 
 
 # ========== Bot Commands ==========
 
-# @bot.command()  # Assignments command
-# async def assignments(ctx):
-#     assignmentsEmbed = discord.Embed(
-#         title="未繳交的作業", description="\t", color=0xF7DFA5)  # Creates the Message Embed
+@bot.command()
+async def hello(ctx):     #say hello
+    await ctx.send(f"hello, {ctx.author.name}")
 
-#     assignments = []
-#     if os.path.exists('savedAssignments.json'):  # If savedAssignments.json exists
-#         with open('savedAssignments.json', 'r') as savedAssignments:
-#             # Read and load savedAssignments.json into assignments[]
-#             assignments = json.load(savedAssignments)
-
-#     # == Get Assignment List and Add Fields ==
-
-#     # If savedAssignments.json does not exist (Only when starting the bot for the first time)
-#     else:
-#         # Tells the user to wait for about 2 minutes
-#         return await ctx.send('初次獲取資料需時約2分鐘, 請稍後重新使用指令')
-
-#     # If no courses were found (usually impossible)
-#     if assignments == 'No courses found':
-#         # Tells the user that cannot find the courses
-#         return await ctx.send('找不到課程')
-
-#     # Add the first 24 assignments to the embed as fields
-#     #   P.S. 25 is the maximum limit of fields per Message Embed
-#     for assignment in assignments[:24]:
-#         dueDatetime = timestampFromDue(
-#             assignment) if 'dueDate' in assignment else '無截止日期'
-
-#         # Adds a field containing information of the assignment to the Message Embed
-#         assignmentsEmbed.add_field(
-#             name=assignment["title"], value='截止時間: {}\n[開啟作業]({})'.format(dueDatetime, assignment["alternateLink"]), inline=False)
-
-#     # Sends the message containing the Message Embed
-#     return await ctx.send(embed=assignmentsEmbed)
 
 @bot.command()
-async def hello(ctx, arg1='', arg2=''):     #say hello
-    await ctx.send(f"hello, {ctx.author.name}")
+async def env(ctx):     #display environment data
+    await ctx.send('小英為你獲取資料中，請你稍等...')
+    envis=fetchEnvData()
+    # embedFields=[]
+    # for envi in envis:
+    #     embedFields.append('地點：{}'&envi('location'))
+    # await ctx.send(embedFields)
 
 
 @bot.command()
---
+++ env.py
@@ -0,0 +1,77 @@
+from datetime import datetime, timedelta
+import urllib.request
+import json
+import ssl
+
+
+def fetchEnvData() -> dict:
+    """Get the timetable from "https://iot.spyc.hk/spycenv"
+
+    Returns:
+        dict: dictionary containing all classes' timetable
+    """
+
+    ssl._create_default_https_context = ssl._create_unverified_context
+    with urllib.request.urlopen("https://iot.spyc.hk/spycenv") as url:
+        results = url.read().decode()
+        data = json.loads(results)
+
+        # save data as /timetable.json
+
+
+        # Return timetable
+        print(data)
+        return data
+
+
+# def getLessonList(class_: str, day: str) -> list:
+#     """Get lesson list for the given class and the given day
+
+#     Args:
+#         class_ (str): the given class
+#         day (str): the given day
+
+#     Returns:
+#         list: list of subjects
+#     """
+
+#     lessonsJSON = fetchAPI()
+#     # If the the lessonsJSON contains the class we want to get
+#     # and the day we want to get
+#     if class_ in lessonsJSON:
+#         if day in lessonsJSON[class_]:
+#             lessons = lessonsJSON[class_][day]
+#         else:
+#             return None, "不存在此 Day"
+#     else:
+#         return None, "找不到此班別"
+
+#     subjects = []
+#     for lesson in lessons:
+#         subjects.append(lesson["subject"])
+
+#     return subjects
+
+
+# def getDayOfCycle(tomorrow: bool) -> str:
+#     """Get Day of Cycle of today
+
+#     Returns:
+#         str: Day of cycle (A-H)
+#     """
+#     now = datetime.now() 
+#     queryDatetime = now + timedelta(days=1) if tomorrow else now
+    
+#     date = queryDatetime.strftime('%a %b %d %Y')
+
+#     ssl._create_default_https_context = ssl._create_unverified_context
+#     with urllib.request.urlopen("https://iot.spyc.hk/cyclecal") as url:
+#         results = url.read().decode()
+#         data = json.loads(results)
+#         dayOfCycle = data[date]
+#         # print(dayOfCycle)
+#         return dayOfCycle
+
+
+if __name__ == "__main__":
+    fetchEnvData()
